cdbb44fb2cad3988548db0b608dcc1fdbb6d05ca
simeone <deweixu@simeonedeMacBook-Pro.local>
2023-04-02 13:24:46 +0800

    use class

--- moe_to_star.py
+++ moe_to_star.py
@@ -3,68 +3,67 @@
 import json
 import os
 
-# {"word": [0, 12]}
-moe_idx_dict = {}
-moe_dict = b''
-offset = 0
 
+class StarFormat:
+    def __init__(self, json_path):
+        self.json_path = json_path
+        # {"word":[0, 10]}
+        self.moe_idx_dict = {}
+        # dict bytes
+        self.moe_dict = b''
+        self.offset = 0
 
-def ifo_file():
-    print("==== gen ifo file ====")
-    content = "StarDict's dict ifo file\n"
-    content += "version=2.4.2\n"
-    content += "wordcount="
-    content += str(len(moe_idx_dict))
-    content += "\n"
-    content += "idxfilesize="
-    idx_file_size = os.path.getsize("./moe.idx")
-    content += str(idx_file_size)
-    content += "\n"
-    content += "bookname=萌典\n"
-    content += "sametypesequence=m\n"
-    with open('./moe.ifo', 'a') as out:
-        out.write(content)
+    def idx_file(self):
+        print("==== gen idx file ====")
+        with open('./moe.idx', 'ab') as out:
+            for k in self.moe_idx_dict:
+                out.write(k.encode('utf-8'))
+                out.write(b'\x00')
+                arr = self.moe_idx_dict[k]
+                out.write(arr[0].to_bytes(4, 'big'))
+                out.write(arr[1].to_bytes(4, 'big'))
 
+    def dict_file(self):
+        print("==== gen dict file ====")
+        with open('./moe.dict', 'ab') as out:
+            out.write(self.moe_dict)
 
-def idx_file():
-    print("==== gen idx file ====")
-    with open('./moe.idx', 'ab') as out:
-        for k in moe_idx_dict:
-            out.write(k.encode('utf-8'))
-            out.write(b'\x00')
-            arr = moe_idx_dict[k]
-            out.write(arr[0].to_bytes(4, 'big'))
-            out.write(arr[1].to_bytes(4, 'big'))
+    def ifo_file(self):
+        print("==== gen ifo file ====")
+        content = "StarDict's dict ifo file\n"
+        content += "version=2.4.2\n"
+        content += "wordcount="
+        content += str(len(self.moe_idx_dict))
+        content += "\n"
+        content += "idxfilesize="
+        idx_file_size = os.path.getsize("./moe.idx")
+        content += str(idx_file_size)
+        content += "\n"
+        content += "bookname=萌典\n"
+        content += "sametypesequence=m\n"
+        with open('./moe.ifo', 'a') as out:
+            out.write(content)
 
-
-def dict_file():
-    print("==== gen dict file ====")
-    with open('./moe.dict', 'ab') as out:
-        out.write(moe_dict)
-
-
-def parse_moe_json():
-    f = open("./dict-revised.json")
-    data = json.load(f)
-    lc = 0
-    for word in data:
-        title = word['title']
-        # skip PUA code
-        if '{' in title:
-            continue
-        data = word_data(word)
-        global moe_dict
-        data_bytes = data.encode('utf-8')
-        size = len(data_bytes)
-        global offset
-        arr = [offset, size]
-        moe_idx_dict[title] = arr
-        moe_dict += data_bytes
-        offset = len(moe_dict)
-        lc += 1
-        if lc % 10000 == 0:
-            print("Deal word: " + str(lc))
-    f.close()
+    def parse_moe_json(self):
+        f = open(self.json_path)
+        data = json.load(f)
+        lc = 0
+        for word in data:
+            title = word['title']
+            # skip PUA code
+            if '{' in title:
+                continue
+            data = word_data(word)
+            data_bytes = data.encode('utf-8')
+            size = len(data_bytes)
+            arr = [self.offset, size]
+            self.moe_idx_dict[title] = arr
+            self.moe_dict += data_bytes
+            self.offset = len(self.moe_dict)
+            lc += 1
+            if lc % 10000 == 0:
+                print("Deal word: " + str(lc))
+        f.close()
 
 
 def word_data(word):
@@ -111,9 +110,8 @@ def word_data(word):
 
 
 if __name__ == '__main__':
-    parse_moe_json()
-    idx_file()
-    dict_file()
-    ifo_file()
-
-
+    star_dict = StarFormat("./dict-revised.json")
+    star_dict.parse_moe_json()
+    star_dict.idx_file()
+    star_dict.dict_file()
+    star_dict.ifo_file()
